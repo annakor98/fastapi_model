aaaa03d4fd926f19fb10ada770e03099fbabde58
annakor98 <koryazhkina.am@phystech.edu>
2023-10-18 23:51:35 +0300

    Add files

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,5 @@
+"""Test main"""
+
 from fastapi.testclient import TestClient
 
 from app.main import app
@@ -6,6 +8,7 @@ client = TestClient(app)
 
 
 def test_hello():
+    """Test welcome endpoint"""
     response = client.get('/')
     assert response.status_code == 200
     assert 'application/json' in response.headers['content-type']
