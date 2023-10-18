e7e5bb747b0d87f7b46b2cd08c4d31186d12247d
annakor98 <koryazhkina.am@phystech.edu>
2023-10-18 23:49:28 +0300

    Add files

---
+++ tests/test_main.py
@@ -0,0 +1,12 @@
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+client = TestClient(app)
+
+
+def test_hello():
+    response = client.get('/')
+    assert response.status_code == 200
+    assert 'application/json' in response.headers['content-type']
+    assert response.json()["message"] == "Hello! This is the Iris predictor."
